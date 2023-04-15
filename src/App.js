844cfc9dcba13b02a9428e78fe5018f074e18561
dhruv9316 <vdhruv876@gmail.com>
2023-04-14 21:14:54 -0700

    innitial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import {apiUrl, filterData} from "./data"
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import { toast } from "react-toastify";
+import Spinner from "./components/Spinner";
+
+const App = () => {
+
+  const[courses, setCourses] = useState(null);
+  const[loading, setLoading] = useState(true);
+  const[category, setCategory] = useState(filterData[0].title);
+
+
+  useEffect( () => {
+    const fetchData = async() => {
+      setLoading(true);
+      try{
+        const res = await fetch(apiUrl);
+        const output = await res.json();  
+        setCourses(output.data); 
+      }
+      catch(error){
+        toast.error("Something went wrong !!");
+      }
+      setLoading(false);
+    }   
+    fetchData();
+  },[] );
+
+  return (
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+      <div>
+        <Navbar/>
+      </div>
+      <div>
+        <Filter 
+          filterData = {filterData}
+          category={category}
+          setCategory={setCategory}
+        />
+      </div>
+      <div className="w-11/12 max-w-[1200px] 
+        mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+        {
+          loading ? (<Spinner/>) : (  
+            <Cards 
+              courses={courses}
+              category={category}
+            />)
+        }
+
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,38 @@
+import Card from "./Card";
+import React from 'react'
+import { useState } from 'react';
+
+const Cards = (props) => {
+    let courses = props.courses;
+    let category = props.category;
+    const [likedCourses, setLikedCourses] = useState([]);
+
+    let allCourses = [];
+
+    const getCourses = () => {
+        if(category === "All"){
+            Object.values(courses).forEach( (courseCategory) => {
+                courseCategory.forEach( (course) => {
+                    allCourses.push(course);
+                })
+            });
+            return allCourses;
+        }
+        else{
+            return courses[category];
+        }
+    }
+
+  return (
+    <div className="flex flex-wrap justify-center gap-4 mb-4">
+        {
+            getCourses().map( (course) => {
+                    return <Card key={course.id} likedCourses={likedCourses}
+                     course={course} setLikedCourses={setLikedCourses} />
+            })
+        }
+    </div>
+  )
+}
+
+export default Cards
